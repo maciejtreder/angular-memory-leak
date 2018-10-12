2261ce251263bdb2001519fa745e8c857dac830b
Maciej Treder <contact@maciejtreder.com>
2018-10-12 17:08:07 +0200

    angular_memory_leak_step2

--- src/app/lucky/lucky.component.ts
+++ src/app/lucky/lucky.component.ts
@@ -1,24 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { LuckyService } from './lucky.service';
+import { Subscription } from 'rxjs';
 
 @Component({
  template: `
    <p>You are checking if you are lucky {{displayCount}} time</p>
-
    <p>Your lucky number is: {{number}}</p>
- `,
+`,
 })
-export class LuckyComponent implements OnInit {
+export class LuckyComponent implements OnInit, OnDestroy {
  public subscribersCount = 0;
  public number: number;
+
+ private luckySubscription$: Subscription;
 
  constructor(private luckyService: LuckyService) {}
 
  public ngOnInit(): void {
-   this.luckyService.getLuckyNumber().subscribe((luckyNumber: number) => {
-     this.number = luckyNumber;
-     console.log(`Retrieved lucky number ${this.number} for subscriber ${this.subscribersCount}`);
+   this.luckySubscription$ = this.luckyService.getLuckyNumber().subscribe((luckyNumber: number) => {
+    this.number = luckyNumber;
+    console.log(`Retrieved lucky number ${this.number} for subscriber ${this.subscribersCount}`);
    });
    this.subscribersCount = this.luckyService.getSubscribersCount();
+ }
+
+ public ngOnDestroy(): void {
+   this.luckySubscription$.unsubscribe();
  }
 }
